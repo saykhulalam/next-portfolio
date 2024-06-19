db8f37fc842687d572896e50d2965cfa5552d019
saykhul alam <156447267+saykhulalam@users.noreply.github.com>
2024-06-19 18:55:02 +0600

    done

--- app/component/Loading.jsx
+++ app/component/Loading.jsx
@@ -7,7 +7,7 @@ const Loading = () => {
       <h2 className="text-white text-[30px] font-poppins font-bold mb-4">
         Loading...
       </h2>
-      <div className="w-full max-w-md h-2 bg-gray-300 rounded-full overflow-hidden">
+      <div className="w-[80%] max-w-md h-2 bg-gray-300 rounded-full overflow-hidden">
         <div className="h-full bg-blue-500 animate-loadingBar"></div>
       </div>
     </div>
